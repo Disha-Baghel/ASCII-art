fc53835014b001a5d9c069f65d030bf861dbe263
Disha-Baghel <bagheldisha708@gmail.com>
2023-01-15 23:48:59 +0530

    Added new file

---
+++ main.cpp
@@ -0,0 +1,29 @@
+
+// #include <opencv2/core.hpp>
+#include <opencv4/opencv2/opencv.hpp>
+// #include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+// #include <stdio.h>
+using namespace cv;
+int main(){
+    Mat image = imread("ascii-pineapple.jpg", IMREAD_COLOR);
+    
+    // int b, g, r;
+    // for(int i=0; i<image.rows;i++){
+    //     for(int j=0; j<image.cols; j++){
+    //         b = image.at<Vec3b>(i, j)[0];
+    //         g = image.at<Vec3b>(i, j)[1];
+    //         r = image.at<Vec3b>(i, j)[2];
+    //     }
+    // }
+
+    if(image.empty()){
+        std::cout<<"Could not read the image: "<<std::endl;
+    }
+
+    imshow("Display window", image);
+
+    if(waitKey(0) == 27){
+        std::cout<<"Image read successfully"<<std::endl;
+    }
+}
