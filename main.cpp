47cd1761d6438ccae17b382dd00751e7cf9dedec
Disha-Baghel <bagheldisha708@gmail.com>
2023-01-21 21:07:51 +0530

    Added new file

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@ using namespace cv;
 int main(){
     Mat image = imread("ascii-pineapple.jpg", IMREAD_COLOR);
     
-    // int b, g, r;
-    // for(int i=0; i<image.rows;i++){
-    //     for(int j=0; j<image.cols; j++){
-    //         b = image.at<Vec3b>(i, j)[0];
-    //         g = image.at<Vec3b>(i, j)[1];
-    //         r = image.at<Vec3b>(i, j)[2];
-    //     }
-    // }
+    int b, g, r;
+    for(int i=0; i<image.rows;i++){
+        for(int j=0; j<image.cols; j++){
+            b = image.at<Vec3b>(i, j)[0];
+            g = image.at<Vec3b>(i, j)[1];
+            r = image.at<Vec3b>(i, j)[2];
+        }
+    }
 
     if(image.empty()){
         std::cout<<"Could not read the image: "<<std::endl;
